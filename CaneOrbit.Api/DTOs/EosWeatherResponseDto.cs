3c9a6c6955fa0fd25ad02377258a0e0d2c5392bd
Juliacsou <julia.correa.souza@gmail.com>
2026-06-04 02:44:38 -0300

    feat: adicionar dto de resposta weather da eos

---
+++ CaneOrbit.Api/DTOs/EosWeatherResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CaneOrbis.Api.DTOs
+{
+    public class EosWeatherResponseDto
+    {
+        public decimal? Precipitacao { get; set; }
+
+        public decimal? TemperaturaAr { get; set; }
+
+        public string? CondicaoClima { get; set; }
+
+        public string? Mensagem { get; set; }
+    }
+}
